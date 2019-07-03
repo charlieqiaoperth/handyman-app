a001718377460c94af5734a085a4bd8b569f20fa
silverwind <charlieqiaoperth@gmail.com>
2019-07-03 23:03:33 +0800

    Updates

---
+++ precache-manifest.d1e35eda1068a1cfde0dcfe2f5c8b483.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daadba2b9487bc114fc046f7c3ebe98d",
+    "url": "/handyman-app/index.html"
+  },
+  {
+    "revision": "cb196314abdc9976571b",
+    "url": "/handyman-app/static/css/2.7355e6c0.chunk.css"
+  },
+  {
+    "revision": "d2b01bc565e8e0c4d953",
+    "url": "/handyman-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cb196314abdc9976571b",
+    "url": "/handyman-app/static/js/2.17218d2b.chunk.js"
+  },
+  {
+    "revision": "d2b01bc565e8e0c4d953",
+    "url": "/handyman-app/static/js/main.97448847.chunk.js"
+  },
+  {
+    "revision": "a83dee7d8a379a1e747b",
+    "url": "/handyman-app/static/js/runtime~main.cd784a0c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/handyman-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/handyman-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/handyman-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/handyman-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/handyman-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
